840c6f64d191dcc5d9b139921a0b77c447c16ba6
Chantelle <115734837+ChantelleAA@users.noreply.github.com>
2024-05-26 20:05:45 +0300

    Add files via upload

---
+++ RuleEngine.py
@@ -0,0 +1,83 @@
+import numpy as np
+
+class RuleEngine:
+    """
+    Manages and applies the rules of the game, determining the validity of actions and the conditions for stopping rounds and the game.
+
+    This class is responsible for checking if actions taken by players are valid, determining when a round or the game should end based on the state of the board and game rules.
+
+    Attributes:
+        B (Board): An instance of the Board class, which represents the game board and its state.
+        state (GameState): An instance of the GameState class, used to track and manage the current state of the game.
+        round (int): The current round number in the game.
+        turn (int): The current turn number within the round.
+        actions (np.ndarray): An array of possible action indices, usually representing pits on the board.
+
+    Methods:
+        __init__(board, state): Initializes the rule engine with references to the game board and state.
+        is_valid_action(action, player): Determines if a given action is valid for a player at the current state.
+        stop_round(): Checks if the current round should stop based on the game board's state.
+        stop_game(): Determines if the game should be stopped, typically based on a win condition or other rule.
+        check_round_end(): Verifies if the round should end, generally due to a specific board state.
+    """
+
+    def __init__(self, board, state):
+        """
+        Initializes the RuleEngine with necessary components of the game.
+
+        Parameters:
+            board (Board): The game board which holds the state and configuration of seeds and pits.
+            state (GameState): The object managing the overall state of the game, including scores and turns.
+        """
+        self.board = board
+        self.state = state
+        self.round = 1
+        self.turn = 1
+        self.actions = np.arange(12)
+
+    def is_valid_action(self, action, player):
+        """
+        Checks if a specific action is valid for a given player based on the current game state.
+
+        Parameters:
+            action (int): The action to check, typically an index representing a pit.
+            player (int): The player number performing the action.
+
+        Returns:
+            bool: True if the action is valid, False otherwise.
+        """
+        # check if the action is valid for a player
+        return action in self.state.possible_moves(player, self.board.board)
+
+    def stop_round(self):
+        """
+        Determines if the current round should be stopped, typically when no seeds are left to play.
+
+        Returns:
+            bool: True if the board is empty and the round should stop, False otherwise.
+        """
+        early_stop = (self.board.stores[0] > 24 and self.board.stores[1] < 24) or (self.board.stores[1] > 24 and self.board.stores[0] < 24)
+        stop_criteria = np.sum(self.board.board, axis=None) == 0
+        return early_stop or stop_criteria
+
+    # def stop_round(self):
+    #     """
+    #     Determines if the current round should be stopped, typically when no seeds are left to play.
+
+    #     Returns:
+    #         bool: True if the board is empty and the round should stop, False otherwise.
+    #     """
+    #     return np.sum(self.board.board, axis=None) == 0
+
+    def stop_game(self, num_rounds):
+        """
+        Checks if the game should end, usually based on a significant victory condition or rule, such as a player achieving a specific territory count.
+
+        Returns:
+            bool: True if the game should end, for example, if a player's territory count reaches a set threshold.
+        """
+        condition1 = self.board.territory_count[1] == 11 or self.board.territory_count[0] == 11 
+        condition2 = self.board.territory_count[1] == 7 or self.board.territory_count[0] == 7 
+
+        return condition2 
+        # return self.board.territory_count[1] == 12 or self.board.territory_count[0] == 12 or self.round == num_rounds
---
+++ __init__.py
@@ -0,0 +1,12 @@
+from .Board import Board
+from .GameState import GameState
+from .Player import Player
+from .RuleEngine import RuleEngine
+from .GameController import GameController
+from agent.DQNAgent import DQNAgent
+from agent.RandomAgent import RandomAgent
+from utils.config import *
+from utils.utils import *
+from agent.DDQNAgent import DDQNAgent
+# from agent.A3C import A3CAgent
+from agent.AlphaZero import AlphaZeroAgent, mcts_policy
